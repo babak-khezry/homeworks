ee11a95642e9f3685d5fad2b25de53128410624e
babak khezri <babakkhezri7@gmail.com>
2020-01-04 00:17:09 +0330

    new changes for delete acounts

--- atm-simulation.c
+++ atm-simulation.c
@@ -8,10 +8,11 @@ struct customer//sturct
 	char accn[max];
 	char fname[20];
 	char lname[20];
-	int accp;
+	int accp;//account password
 	int balance;
-	int news[max][2];
+	int updates;//show the account turnovers
 };
+int iNumber = 0;//to check the number of customers that we have
 struct customer cost[100];//variable of all costomers
 void changePasword(int);//change the pasword
 void accessAcc();//access to the account
@@ -19,25 +20,29 @@ void transfer(int);//transfer money betwen accounts
 void balance(int);//show the money
 void recentUpdate(int);//show the 10 last updates(turnovers)
 void employAcc();//get access to developer
-void menu();//menu
-void deleteAcc();
+void menu();//list of main menu
+void deleteAcc();//delete accounts by change thier money to 0
 void updateAcc();//update the account by employer
 void employe();//employer menu
 void custom(int);//customer menu
 void addacc();//create the new accounts
 void cash(int);//get cash money
-int end();//end the program
-//more info
-
+void end();//end the program
+void listArchiv();//list of removed acounts
 int main()
 {
 	int i;
+	for(i=0;i<100;i++)
+		cost[i].updates=0;
+	for(i=0;i<100;i++)
+		cost[i].balance=1;//put 1 balance for all the accounts couse of acc acces
 	FILE *ptf;//file
 	ptf = fopen("C:\\Users\\Radin\\Desktop\\project\\text.txt","r+");//geting file
 	for(i=0;i<2;i++)
 	{	
 		fscanf(ptf,"%s %s %s",cost[i].fname,cost[i].lname,cost[i].accn);//geting file informations
 		fscanf(ptf,"%d %d",&cost[i].accp,&cost[i].balance);//geting file informations
+		fscanf(ptf,"%d",&iNumber);
 	}
 	menu();
 	return 0;
@@ -83,6 +88,8 @@ void employe()
    			 		break;
     			case 3 : deleteAcc();//remove account
 					break;
+				case 4 : listArchiv();//list of remove accounts	
+					break;
 				case 5 : menu();//back to menue
 					break;
 			}
@@ -96,22 +103,21 @@ void custom(int n)
 	printf("1. Account balance\n2. Transfer\n");//menu
 	printf("3. Cash withdrawal\n4. Recent account updates\n");
 	printf("5. Change password\n6. Back to the main menu\n");
-	int copy = n ;
-	int x;
+	int copy = n, x;
 	scanf("%d",&x);
 	switch(x)
     {
-    	case 1 : balance(copy);
+    	case 1 :	balance(copy);
     		break;
-    	case 2 : transfer(copy);
+    	case 2 :	transfer(copy);
     		break;
-    	case 3 : cash(copy);
+    	case 3 :	cash(copy);
 			break;
-		case 4 : recentUpdate(copy);
+		case 4 :	recentUpdate(copy);
 			break;
-		case 5 : changePasword(copy);
+		case 5 :	changePasword(copy);
 			break;
-		case 6 : menu();
+		case 6 :	menu();
 	}
 }
 void addacc()//create the acounts
@@ -120,9 +126,10 @@ void addacc()//create the acounts
 	int n ,i;
 	printf("Enter how much account you want to create : ");//the number of new acounts
 	scanf("%d",&n);
-	for(i=2;i<n+2;i++)//put accounts
+	n+=iNumber;
+	for(i = iNumber; i<n; i++)//put accounts
 	{
-		printf("Enter the name : ");
+		printf("Enter the first name : ");
 		scanf("%s",cost[i].fname);
 		printf("Enter the last name : ");
 		scanf("%s",cost[i].lname);
@@ -139,8 +146,10 @@ void addacc()//create the acounts
 		}	
 	printf("Enter the lnitial balance costomer <%d> : ",i+1);
 	scanf("%d",&cost[i].balance);//put balance
+	
 	system("cls");
 	}
+	iNumber = n;
 	printf("press Enter to go back to menu : ");//going back to menu
 	getchar();
 	getchar();
@@ -155,7 +164,7 @@ void balance(int copy)//show money available
 	for(;;)
 	{	
 		scanf("%d",&y);
-		if(y==1||y==2)
+		if(y == 1||y == 2)
 			switch(y)
 			{
 				case 1 : custom(copy);//got back to costomer menu
@@ -167,15 +176,14 @@ void balance(int copy)//show money available
 }
 void accessAcc()//customer acces
 {
-	int copy;//number of account
-	int i, pas ,count = 0;
+	int i, pas, count = 0, copy;//copy = number of account 
 	system("cls");
 	char accn[max];
 	printf("Enter your card number : ");
 	for(;;)//cheack that the number is excist or not
 	{
 		scanf("%s",accn);
-		for(i=0;i<100;i++)//compare with all
+		for(i=0 ;i<100 ;i++)//compare with all
 		{
 			if(strcmp(accn,cost[i].accn)==0)//finding acount
 			{
@@ -186,17 +194,16 @@ void accessAcc()//customer acces
 		copy = i;//number of account
 		if (count == 0)
 			printf("The acount does not exist\nEnter the account again : ");
-		if (cost[copy].balance==0)//check the account is removed or not
-			{
-				printf("Account has been deleted");
-				recentUpdate(copy);
-			}
+		if (cost[copy].balance==0&&count==1)//check the account is removed or not
+		{
+			printf("Account has been deleted");
+			recentUpdate(copy);
+		}
 		if (count == 1)
-		
 		{
 			printf("Enter pasword : ");//put pasword
 			scanf("%d",&pas);
-			if(pas==cost[copy].accp)//check pasword
+			if(pas == cost[copy].accp)//check pasword
 				custom(copy);
 		}		
 	}
@@ -204,21 +211,20 @@ void accessAcc()//customer acces
 void changePasword(int copy)//put new password
 {
 	system("cls");
-	int pas;
-	printf("Enter the new password : ");
+	int pas, y;
+	printf("Enter the new password : ");//put the new password
 	for(;;)
 	{
-		scanf("%d",&pas);
-		if(pas>999) break;
+		scanf("%d",&pas);//check that pass is more then 4 number
+		if(pas > 999) break;
 		printf("short pasword ! \nput another pasword");
 	}
-	cost[copy].accp=pas;
+	cost[copy].accp=pas;//chenge password
 		printf("\nAny another request : \n1_yes\n2_no \n");
-	int y;
 	for(;;)
 	{	
 		scanf("%d",&y);
-		if(y==1||y==2)
+		if(y == 1||y == 2)
 			switch(y)
 			{
 				case 1 : custom(copy);//got back to costomer menu
@@ -232,9 +238,9 @@ void cash(int copy)//get the cash money
 {
 	system("cls");
 	printf("Enter how much you want to withdraw : ");
-	int money;
+	int money,y;
 	scanf("%d",&money);//put how much cash you want to get
-	if(money>cost[copy].balance)
+	if(money > cost[copy].balance)
 	{
 		printf("Not enough inventory");
 		printf("\nAny another request : \n1_yes\n2_no \n");
@@ -249,15 +255,15 @@ void cash(int copy)//get the cash money
 	}
 	else
 	{
-		printf("\n\ntake your money");
-		cost[copy].balance-=money;
-		printf("\n\nthe left over is : %d",cost[copy].balance);
+		printf("\n\ntake your money");//get money 
+		cost[copy].balance -= money;//low off money
+		cost[copy].updates++;
+		printf("\n\nthe left over is : %d",cost[copy].balance);//show the money left
 		printf("\n\nAny another request : \n1_yes\n2_no \n");
-		int y;
 		for(;;)
 		{	
 			scanf("%d",&y);
-			if(y==1||y==2)
+			if(y == 1||y == 2)
 			switch(y)
 			{
 				case 1 : custom(copy);//got back to costomer menu
@@ -268,35 +274,45 @@ void cash(int copy)//get the cash money
 		}
 	}
 }
-int end()//end the program
+void end()//end the program
 {
 	exit(0);//ending the program
 }
 void updateAcc()//update an acount 
 {
-	int i, count = 0 ,pas;
+	int i, count = 0, pas;
 	system("cls");
 	char accn[max];
 	printf("Enter the card number : ");
 	for(;;)//cheack that the number is excist or not
 	{
 		scanf("%s",accn);
-		for(i=0;i<100;i++)//compare with all
+		for(i=0 ; i<100 ; i++)//compare with all
 		{
-			if(strcmp(accn,cost[i].accn)==0)//finding acount
+			if(strcmp(accn,cost[i].accn) == 0)//finding acount
 			{
 				count++;
 				break;
-			}	
+			}
 		}
+		if(count == 0)
+		{
+			printf("The account does not exist");
+			printf("\nEnter the card number again : ");
+			continue;//go to first
+		}
+		printf("Enter the new first name : ");
+		scanf("%s",cost[i].fname);//chenge first name
+		printf("Enter the new last name : ");
+		scanf("%s",cost[i].lname);//chenge last name
 		printf("Enter the new pasword for the account : ");
 		for(;;)//check to pasword be bigger then 3 character
 		{
 		scanf("%d",&pas);
-		if(pas>999) break;
+		if(pas > 999) break;//check password
 		printf("short pasword ! \nput another pasword");
 		}
-		cost[i].accp=pas;
+		cost[i].accp = pas;
 		printf("Press enter for going back to employ menu : ");
 		getchar();//to cancel the last Enter
 		getchar();
@@ -308,15 +324,15 @@ void updateAcc()//update an acount
 void employAcc()//check access of the employer
 {
 	system("cls");
-	char n[max]="admin", n1[max];//user name of employer
-	char p[max]="admin", p1[max];//pasword of employer
+	char n[max] = "admin", n1[max];//user name of employer
+	char p[max] = "admin", p1[max];//pasword of employer
 	for(;;)
 	{
 		printf("Enter the usename : ");
 		scanf("%s",n1);//get null usename
 		printf("Enter the pasword : ");
 		scanf("%s",p1);//get null pasword
-		if(strcmp(n,n1)==0&&strcmp(p,p1)==0)//check the user name and password
+		if(strcmp(n,n1) == 0 && strcmp(p,p1) == 0)//check the user name and password
 			employe();
 		else
 			printf("wrong username or pasword\n");
@@ -326,12 +342,12 @@ void transfer(int copy)//transfer money betwen accounts
 {
 	system("cls");
 	char accn2[max];
-	int i, copy2, count = 0;
+	int i, copy2, count = 0, money;
 	printf("Enter the destination number : ");
 	scanf("%s",accn2);
 	for(i=0;i<100;i++)//compare with all
 	{
-		if(strcmp(accn2,cost[i].accn)==0)//finding acount
+		if(strcmp(accn2,cost[i].accn) == 0)//finding acount
 		{
 			copy2 = i;//number of distention account
 			break;
@@ -339,9 +355,8 @@ void transfer(int copy)//transfer money betwen accounts
 			
 	}
 	printf("Enter the amount of money : ");
-	int mon;
-	scanf("%d",&mon);
-	if(cost[copy].balance<mon)//checking account have anouph money to tranfer
+	scanf("%d",&money);
+	if(cost[copy].balance < money)//checking account have anouph money to tranfer
 	{
 		printf("not enouph inventory ! ");
 		printf("\nAny another request : \n1_yes\n2_no \n");
@@ -359,7 +374,7 @@ void transfer(int copy)//transfer money betwen accounts
 			else	continue;
 		}
 	}
-	printf("transfer  %d  to < %s >",mon,cost[copy2].accn);//transfer information
+	printf("transfer  %d  to < %s >",money,cost[copy2].accn);//transfer information
 	printf("\n          %s  %s\n\n",cost[copy2].fname,cost[copy2].lname);//transfer information
 	printf("1_confirmation\n2_go back\n");//confirmation the transfer
 	int z;
@@ -368,8 +383,9 @@ void transfer(int copy)//transfer money betwen accounts
 	{
 		case 1 : 
 		{
-			cost[copy].balance-=mon;//reduce money for origin card
-			cost[copy2].balance+=mon;//move money to distention card
+			cost[copy].balance -= money;//reduce money for origin card
+			cost[copy2].balance += money;//move money to distention card
+			cost[copy].updates++;
 			break;
 		}
 		case 2 : end();
@@ -380,12 +396,12 @@ void transfer(int copy)//transfer money betwen accounts
 	for(;;)
 	{	
 		scanf("%d",&y);
-		if(y==1||y==2)
+		if(y == 1||y == 2)
 			switch(y)
 			{
-				case 1 : custom(copy);//got back to costomer menu
+				case 1 :	custom(copy);//got back to costomer menu
 					break;
-				case 2 : end();//close program	
+				case 2 :	end();//close program	
 			}
 		else	continue;
 	}
@@ -395,8 +411,71 @@ void recentUpdate(int copy)//show last 10 updates
 {
 	
 }
-void deleteAcc()
+void deleteAcc()//delect accounts
 {
-	
+	int i, count = 0, x;
+	system("cls");
+	char accn[max];
+	printf("Enter the card number : ");//get the card number
+	for(;;)
+	{
+		scanf("%s",accn);
+		for(i=0;i<100;i++)//compare with all
+		{
+			if(strcmp(accn,cost[i].accn) == 0)//finding acount
+			{
+				count++;
+				break;
+			}
+		}
+		if(count == 1);//find the account
+			break;
+		printf("The acount does not exist\n");
+		printf("Enter the account again : ");
+	}
+	system("cls");
+	printf("The account < %s > ",cost[i].accn);//the acount informations
+	printf("by name ( %s %s )",cost[i].fname,cost[i].lname);//the acount informations
+	printf("\nAre you sure you want to delete this acount :");
+	printf(" \n1_Yes\n2_No\n3_Go to employee menu\n");//request permission
+	for(;;)
+	{
+		scanf("%d",&x);
+		if(x == 1||x == 2||x == 3)
+		switch(x)
+		{
+			case 1 : 
+			{	
+				cost[i].balance = 0;//delete account by put tha balance 0;
+				printf("Account deleted\n");
+				printf("/n/nPress Enter to go back menu");
+				getchar();
+				getchar();	
+				employe();//going back to menu	
+				break;
+			}
+			case 2 : deleteAcc();//change the account that want to remove
+			break;
+			case 3 : employe();//go back to menu
+			break;
+		}	
+	}
 }
-
+void listArchiv()//show the list of removed accounts
+{
+	system("cls");
+	int i, count = 0;
+	for(i=0 ; i<99 ; i++)//check all acounts
+	{
+		if(cost[i].balance == 0)//find accounts whose money is zero
+		{
+			count++;//to make list
+			printf("%d_account number < %s >",count, cost[i].accn);//acount informations
+			printf(" by name %s %s\n",cost[i].fname, cost[i].lname);//acount informations
+		}
+	}
+	printf("Push Enter to go back menu");
+	getchar();
+	getchar();
+	employe();//go back to menu
+}
